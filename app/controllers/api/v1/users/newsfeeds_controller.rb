2ca111a23faa71f38612e58c740f0f26fdb1cbd7
Aldrin Dela Cruz <adelacruz0930@gmail.com>
2023-04-23 23:38:40 +0800

    Add followed sleep logs for the last week in newsfeed

--- app/controllers/api/v1/users/newsfeeds_controller.rb
+++ app/controllers/api/v1/users/newsfeeds_controller.rb
@@ -2,12 +2,20 @@ class Api::V1::Users::NewsfeedsController < Api::V1::BaseController
   before_action :set_user, only: %i(index)
   
   def index
-    render json: UserBlueprint.render(@user, view: :with_newsfeed)
+    @logs = @user.following_sleep_logs.where(
+      clock_in: last_week.beginning_of_week..last_week.end_of_week
+    ).order(duration: :desc)
+
+    render json: SleepLogBlueprint.render(@logs)
   end
 
   private
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def last_week
+    DateTime.now.last_week
   end
 end
--- spec/requests/api/v1/users/newsfeeds_spec.rb
+++ spec/requests/api/v1/users/newsfeeds_spec.rb
@@ -14,30 +14,36 @@ RSpec.describe 'newsfeeds requests' do
         response '200', 'newsfeed retrieved' do
           let(:user1) { create(:user) }
           let(:user2) { create(:user) }
+          let(:user3) { create(:user) }
+
           let(:user_id) { user1.id }
-          let!(:log) { create(:sleep_log, user: user2, clock_in: '2022-01-01 10:00 PM') }
+
+          let!(:sleep_log1) { create(:sleep_log, user: user2, clock_in: '2023-01-03 10:00 PM', clock_out: '2023-01-04 3:00 AM', duration: 5.0) }
+          let!(:sleep_log2) { create(:sleep_log, user: user2, clock_in: '2023-01-04 11:00 PM', clock_out: '2023-01-05 3:00 PM', duration: 14.0) }
+          let!(:sleep_log3) { create(:sleep_log, user: user1, clock_in: '2023-01-03 11:00 PM', clock_out: '2023-01-04 3:00 AM', duration: 4.0) }
+          let!(:sleep_log4) { create(:sleep_log, user: user3, clock_in: '2023-01-03 11:00 PM', clock_out: '2023-01-04 3:00 AM', duration: 4.0) }
 
           before do
+            Timecop.freeze(DateTime.parse('2023-01-15 9:00 AM'))
             create(:follow, follower: user1, followed: user2)
           end
 
           run_test! do
             expect(response).to have_http_status(:ok)
-            expect(json_response).to include(
-              id: user1.id,
-              name: user1.name,
-              newsfeed: contain_exactly(
-                include(
-                  id: user2.id,
-                  name: user2.name,
-                  sleep_logs: contain_exactly(
-                    id: log.id,
-                    user_id: user2.id,
-                    clock_in: '2022-01-01 22:00:00 UTC',
-                    clock_out: nil,
-                    duration: '0.0'
-                  )
-                )
+            expect(json_response).to contain_exactly(
+              include(
+                id: sleep_log2.id,
+                clock_in: '2023-01-04 23:00:00 UTC',
+                clock_out: '2023-01-05 15:00:00 UTC',
+                duration: '14.0',
+                user_id: user2.id
+              ),
+              include(
+                id: sleep_log1.id,
+                clock_in: '2023-01-03 22:00:00 UTC',
+                clock_out: '2023-01-04 03:00:00 UTC',
+                duration: '5.0',
+                user_id: user2.id
               )
             )
           end
